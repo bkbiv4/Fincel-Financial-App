eed7de4c0d27fdefb8354548346906d00987c3b8
bkbiv4 <bkbiv4@gmail.com>
2022-06-19 20:59:05 -0400

    CreateAccountView Updated

--- Fincel/Accounts/CreateAccountView.swift
+++ Fincel/Accounts/CreateAccountView.swift
@@ -11,36 +11,40 @@ struct CreateAccountView: View {
     
     @State var accountNameTextField: String = ""
     @State var accountType: String = ""
-    @State var accountBalance: String = "0.0"
+    @State var accountBalance: String = ""
     
     
     var body: some View {
-        VStack (alignment: .leading, spacing: 0) {
-            
-            HStack () {
-                Label("Account Name", image: "Yerd")
-                    .frame(width: 150, height: 15, alignment: .leading)
-                TextField("Account Name", text: $accountNameTextField)
-                    .foregroundColor(Color.green)
-                    .background(Color.blue.opacity(0.25))
-            }
-            
-            HStack () {
-                Label("Account Type", image: "Yerd")
-                    .frame(width: 150, height: 15, alignment: .leading)
-                TextField("Account Type", text: $accountType)
-                    .foregroundColor(Color.green)
-                    .background(Color.blue.opacity(0.25))
-            }
+        NavigationView {
+            VStack (alignment: .leading, spacing: 25) {
+                HStack () {
+                    Label("Account Name", image: "Yerd")
+                        .frame(width: 150, height: 15, alignment: .leading)
+                    TextField("Account Name", text: $accountNameTextField)
+                        .padding(5)
+                        .background(Color.gray.opacity(0.25).cornerRadius(10))
+                }
+                .frame(height: 15.0)
                 
-            HStack () {
-                Label("Account Balance", image: "Yerd")
-                    .frame(width: 150, height: 15, alignment: .leading)
-                TextField("Account Balance", text: $accountBalance)
-                    .foregroundColor(Color.green)
-                    .background(Color.blue.opacity(0.25))
+                HStack () {
+                    Label("Account Type", image: "Yerd")
+                        .frame(width: 150, height: 15, alignment: .leading)
+                    TextField("Account Type", text: $accountType)
+                        .padding(5)
+                        .background(Color.gray.opacity(0.25).cornerRadius(10))
+                }
+                .frame(height: 15)
+                    
+                HStack () {
+                    Label("Account Balance", image: "Yerd")
+                        .frame(width: 150, height: 15, alignment: .leading)
+                    TextField("Account Balance", text: $accountBalance)
+                        .padding(5)
+                        .background(Color.gray.opacity(0.25).cornerRadius(10))
+                }
+                .frame(height: 15)
             }
-                
+            
         }
     }
 }
