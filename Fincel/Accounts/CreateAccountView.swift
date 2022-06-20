0a9c6fd2f0f4a42ebdcc7479a4fab270a579912c
bkbiv4 <bkbiv4@gmail.com>
2022-06-19 22:05:58 -0400

    Updated CreateAccountView

--- Fincel/Accounts/CreateAccountView.swift
+++ Fincel/Accounts/CreateAccountView.swift
@@ -6,19 +6,27 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct CreateAccountView: View {
+    
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Account.accountName, ascending: true)],
+        animation: .default)
+    private var accounts: FetchedResults<Account>
     
     @State var accountNameTextField: String = ""
     @State var accountType: String = ""
     @State var accountBalance: String = ""
     
-    
     var body: some View {
+
         NavigationView {
-            VStack (alignment: .leading, spacing: 25) {
+            VStack (spacing: 25) {
                 HStack () {
-                    Label("Account Name", image: "Yerd")
+                    Label("Account Name", image: "")
                         .frame(width: 150, height: 15, alignment: .leading)
                     TextField("Account Name", text: $accountNameTextField)
                         .padding(5)
@@ -27,7 +35,7 @@ struct CreateAccountView: View {
                 .frame(height: 15.0)
                 
                 HStack () {
-                    Label("Account Type", image: "Yerd")
+                    Label("Account Type", image: "")
                         .frame(width: 150, height: 15, alignment: .leading)
                     TextField("Account Type", text: $accountType)
                         .padding(5)
@@ -36,16 +44,41 @@ struct CreateAccountView: View {
                 .frame(height: 15)
                     
                 HStack () {
-                    Label("Account Balance", image: "Yerd")
+                    Label("Account Balance", image: "")
                         .frame(width: 150, height: 15, alignment: .leading)
                     TextField("Account Balance", text: $accountBalance)
                         .padding(5)
                         .background(Color.gray.opacity(0.25).cornerRadius(10))
                 }
                 .frame(height: 15)
+                Spacer()
             }
+            .padding()
+            .navigationTitle("Create Account")
             
+            .navigationBarItems(
+                trailing:
+                    Button(action: createAccount) {
+                        Text("Save".uppercased())
+                    }
+            )
         }
+    }
+    
+    private func createAccount() {
+        let account = ""
+        
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+        
+        
+        print("Saving The New Account")
+        
+        
     }
 }
 
